60f2f8cf803c25240872f7774e0dccf371b36ba8
nazar360 <mrmohdnazar@gmail.com>
2023-08-31 22:27:07 +0800

    This Is first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var app = express();
+
+app.use(express.static('public'));
+
+//make way for some custom css, js and images
+app.use('/', express.static(__dirname + '/public'));
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/images', express.static(__dirname + '/public/images'));
+app.use('/webfonts', express.static(__dirname + '/public/webfonts'));
+
+var server = app.listen(80, function(){
+    var port = server.address().port;
+    console.log("Server started at http://localhost:%s", port);
+});
